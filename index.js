9a238db9304d386b9d093f387f24c2c33b365232
Mazharul Islam <mazharul430@gmail.com>
2022-11-07 03:46:07 -0800

    added update delete and jwt api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
@@ -13,8 +14,16 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 const run = async ()=>{
     try{
-        const productCollection = client.db('geniousCarDb').collection('products');
-        const servicesCollection = client.db('geniousCarDb').collection('services');
+      const productCollection = client.db('geniousCarDb').collection('products');
+      const servicesCollection = client.db('geniousCarDb').collection('services');
+      const orderCollection = client.db('geniousCarDb').collection('orders');
+
+
+      // token api 
+       app.post('/jwt',(req, res)=>{
+          const user = req.body;
+          console.log(user);
+       })
 
         // get all Services data from database
         app.get('/services', async (req, res)=>{
@@ -30,6 +39,48 @@ const run = async ()=>{
           const query = {_id: ObjectId(id)};
           const selectedService = await servicesCollection.findOne(query);
           res.send(selectedService);
+        })
+
+        // created orders api form database.
+        app.post('/orders', async(req, res)=>{
+          const order = req.body;
+          const result = await orderCollection.insertOne(order);
+          res.send(result);
+        })
+
+        // get orders api from database.
+        app.get('/orders', async(req, res)=>{
+          let query = {};
+          if(req.query.email){
+              query = {
+                  email: req.query.email
+              }
+          }
+          const cursor = orderCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+        })
+
+        // update orders api from database.
+        app.patch('/orders/:id', async (req, res)=>{
+          const id = req.params.id;
+          const status = req.body.status;
+          const query = {_id: ObjectId(id)};
+          const updateDoc = {
+            $set: {
+              status: status
+            }
+          }
+          const result = await orderCollection.updateOne(query, updateDoc);
+          res.send(result)
+        })
+
+        // delete orders api from database.
+        app.delete('/orders/:id', async (req, res)=>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const result = await orderCollection.deleteOne(query);
+          res.send(result);
         })
 
     }
